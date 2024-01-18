dd501240ba8c9ee0b2880997e4d83daf7fd83d9c
Sergio Orozco Palencia <sop13@alu.ua.es>
2024-01-18 21:04:27 +0100

    CRUD de servicios terminado

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -21,6 +22,12 @@ Route::group(['middleware' => ['auth:api']], function () {
         //Mail::to(Auth::user()->email)->send(new Registro(Auth::user()));
         return response()->json('pong', 200);
     });
+
+    Route::get('service', [ServiceController::class, 'index']);
+    Route::get('service/{id}', [ServiceController::class, 'show']);
+    Route::post('service/create', [ServiceController::class, 'store']);
+    Route::put('service/update/{id}', [ServiceController::class, 'update']);
+    Route::get('service/delete/{id}', [ServiceController::class, 'destroy']);
 });
 
 Route::post('login', [UserController::class, 'login']);
