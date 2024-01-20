69ad6dbf4f6f67892a40442c31efb362a11aac1e
Sergio Orozco Palencia <sop13@alu.ua.es>
2024-01-20 15:41:21 +0100

    Migración para cambiar los tipos del precio de los servicios y del descuento de las ofertas

--- database/seeders/OfferSeeder.php
+++ database/seeders/OfferSeeder.php
@@ -12,17 +12,17 @@ class OfferSeeder extends Seeder
     {
         $oferta1 = new Offer();
         $oferta1->name = "5%";
-        $oferta1->discount = "5%";
+        $oferta1->discount = 5;
         $oferta1->save();
 
         $oferta2 = new Offer();
         $oferta2->name = "10%";
-        $oferta2->discount = "10%";
+        $oferta2->discount = 10;
         $oferta2->save();
 
         $oferta3 = new Offer();
         $oferta3->name = "15%";
-        $oferta3->discount = "15%";
+        $oferta3->discount = 15;
         $oferta3->save();
     }
 }
--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -13,19 +13,19 @@ class ServiceSeeder extends Seeder
         $servicio1 = new Service();
         $servicio1->title = "Cama supletoria";
         $servicio1->description = "Se añade una cama supletoria a la habitación reservada.";
-        $servicio1->price = "0";
+        $servicio1->price = 0;
         $servicio1->save();
 
         $servicio2 = new Service();
         $servicio2->title = "Desayuno";
         $servicio2->description = "Desayuno en el hotel.";
-        $servicio2->price = "10";
+        $servicio2->price = 10.2;
         $servicio2->save();
 
         $servicio3 = new Service();
         $servicio3->title = "Cena";
         $servicio3->description = "Cena en el hotel.";
-        $servicio3->price = "15";
+        $servicio3->price = 15;
         $servicio3->save();
     }
 }
